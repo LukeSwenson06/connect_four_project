4a3ad9ba491ec158f08e60d92396e650b2d0bd24
Stephen Wilkens <stephenwilkens@gmail.com>
2022-04-02 13:17:21 -0600

    Feat: adjusted print board method to account for transposed rows instead of columns

--- lib/board.rb
+++ lib/board.rb
@@ -7,13 +7,13 @@ class Board
 
   def initialize(hash_columns = {
 
-    A: ['1', '1', '1', '1', '1', '1'],
-    B: ['2', '2', '2', '2', '2', '2'],
-    C: ['3', '3', '3', '3', '3', '3'],
-    D: ['4', '4', '4', '4', '4', '4'],
-    E: ['5', '5', '5', '5', '5', '5'],
-    F: ['*', '*', '*', '*', '*', '*'],
-    G: ['*', '*', '*', '*', '*', '*'],
+    A: ['1', '2', '3', '4', '5', '6'],
+    B: ['1', '2', '3', '4', '5', '6'],
+    C: ['1', '2', '3', '4', '5', '6'],
+    D: ['1', '2', '3', '4', '5', '6'],
+    E: ['1', '2', '3', '4', '5', '6'],
+    F: ['1', '2', '3', '4', '5', '6'],
+    G: ['1', '2', '3', '4', '5', '6']
     })
     @hash_columns = hash_columns
   end
@@ -23,75 +23,15 @@ class Board
   end
 
 
-
-
   def print_board
+    (row6,row5,row4,row3,row2,row1,) = @hash_columns.values.transpose
 
-    puts "A" "B" "C" "D" "E" "F" "G"
-    a,b,c,d,e,f,g = @hash_columns.values.transpose
-    p a.join(' ')
-    p b.join(' ')
-    p c.join(' ')
-    p d.join(' ')
-    p e.join(' ')
-    p f.join(' ')
-    p g.join(' ')
-    # print hash_columns[:A][6]
-    # print hash_columns[:B][6]
-    # print hash_columns[:C][6]
-    # print hash_columns[:D][6]
-    # print hash_columns[:E][6]
-    # print hash_columns[:F][6]
-    # puts hash_columns[:G][6]
-    # print hash_columns[:A][5]
-    # print hash_columns[:B][5]
-    # print hash_columns[:C][5]
-    # print hash_columns[:D][5]
-    # print hash_columns[:E][5]
-    # print hash_columns[:F][5]
-    # puts hash_columns[:G][5]
-    # print hash_columns[:A][4]
-    # print hash_columns[:B][4]
-    # print hash_columns[:C][4]
-    # print hash_columns[:D][4]
-    # print hash_columns[:E][4]
-    # print hash_columns[:F][4]
-    # puts hash_columns[:G][4]
-    # print hash_columns[:A][3]
-    # print hash_columns[:B][3]
-    # print hash_columns[:C][3]
-    # print hash_columns[:D][3]
-    # print hash_columns[:E][3]
-    # print hash_columns[:F][3]
-    # puts hash_columns[:G][3]
-    # print hash_columns[:A][2]
-    # print hash_columns[:B][2]
-    # print hash_columns[:C][2]
-    # print hash_columns[:D][2]
-    # print hash_columns[:E][2]
-    # print hash_columns[:F][2]
-    # puts hash_columns[:G][2]
-    # print hash_columns[:A][1]
-    # print hash_columns[:B][1]
-    # print hash_columns[:C][1]
-    # print hash_columns[:D][1]
-    # print hash_columns[:E][1]
-    # print hash_columns[:F][1]
-    # puts hash_columns[:G][1]
-    # print hash_columns[:A][0]
-    # print hash_columns[:B][0]
-    # print hash_columns[:C][0]
-    # print hash_columns[:D][0]
-    # print hash_columns[:E][0]
-    # print hash_columns[:F][0]
-    # puts hash_columns[:G][0]
+    puts " A B C D E F G"
+    p row6.join(' ')
+    p row5.join(' ')
+    p row4.join(' ')
+    p row3.join(' ')
+    p row2.join(' ')
+    p row1.join(' ')
   end
-  # hash_columns.map do |key|
-  #   if key == :A ||
 end
-
-
-#   hash_columns[:A].map do |string|
-#     p string
-#   end
-# end
