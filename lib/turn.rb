7cf6b3d42b8c382761567e481ae83f196ae2fe4e
Luke Swenson <LukeSwenson95@outlook.com>
2022-04-02 16:45:27 -0600

    feat:working on code to place pieces

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,13 +5,15 @@ class Turn
   end
 
   def player_turn
-      user_input = ui
-    until ui[5] == ('.')
-      ui = STDIN.gets.chomp.upcase
-    end
-    require'pry';binding.pry
-    user_input.find do |piece|
-        piece.sub('.','O')
-      end
-    end
+    user_input = []
+    ui = STDIN.gets.chomp.upcase.to_sym
+  if @hash_columns[ui][5] == ('.')
+    user_input << ui
   end
+
+    # user_input.find do |piece|
+    #     piece.sub('.','O')
+  end
+end
+
+# cd turing/1Module/projects/connect_four_project/
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -6,12 +6,14 @@ describe 'Turn' do
   let(:player){Player.new("Vob", "X")}
   let(:computer){Player.new("AI", "O")}
   let(:turn){Turn.new(player, computer)}
+  let(:board){Board.new}
 
   it 'exists' do
     expect(turn).to be_a(Turn)
   end
 
   it "can recieve input and check if column is full" do
+      require'pry';binding.pry
     expect(turn.player_turn).to eq(1)
   end
 
