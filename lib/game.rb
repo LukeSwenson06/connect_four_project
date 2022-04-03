89aadb889ac2740d48b2faab20f16d2a73a5d55a
Luke Swenson <LukeSwenson95@outlook.com>
2022-04-03 17:15:42 -0600

    Added Horizontal win conditions

--- lib/game.rb
+++ lib/game.rb
@@ -61,19 +61,86 @@ class Game
     end
   end
 
-  def vertical_win_claus(user_input)
+  def vertical_win_clause(user_input)
    vertical = @board.hash_columns[user_input].find_all_index("X")
    if vertical == [0, 1, 2, 3]
      player_win
    elsif vertical == [1, 2, 3, 4]
      player_win
    elsif vertical == [2, 3, 4, 5]
-     player_win 
+     player_win
    end
  end
     #conditional to win diagonal
     # condition to diagonal
-    def
+  def horizontal_win_clause
+    (row6,row5,row4,row3,row2,row1,) = @board.hash_columns.values.transpose
+    six = row6.find_all_index('X')
+    if six == [0, 1, 2, 3]
+      player_win
+    elsif six == [1, 2, 3, 4]
+      player_win
+    elsif six == [2, 3, 4, 5]
+      player_win
+    elsif six == [3, 4, 5, 6]
+      player_win
+    end
+
+    five = row5.find_all_index('X')
+    if five == [0, 1, 2, 3]
+      player_win
+    elsif five == [1, 2, 3, 4]
+      player_win
+    elsif five == [2, 3, 4, 5]
+      player_win
+    elsif five == [3, 4, 5, 6]
+      player_win
+    end
+
+    four = row4.find_all_index('X')
+    if four == [0, 1, 2, 3]
+      player_win
+    elsif four == [1, 2, 3, 4]
+      player_win
+    elsif four == [2, 3, 4, 5]
+      player_win
+    elsif four == [3, 4, 5, 6]
+      player_win
+    end
+
+    three = row3.find_all_index('X')
+    if three == [0, 1, 2, 3]
+      player_win
+    elsif three == [1, 2, 3, 4]
+      player_win
+    elsif three == [2, 3, 4, 5]
+      player_win
+    elsif three == [3, 4, 5, 6]
+      player_win
+    end
+
+    two = row2.find_all_index('X')
+    if two == [0, 1, 2, 3]
+      player_win
+    elsif two == [1, 2, 3, 4]
+      player_win
+    elsif two == [2, 3, 4, 5]
+      player_win
+    elsif two == [3, 4, 5, 6]
+      player_win
+    end
+
+    one = row1.find_all_index('X')
+    if one == [0, 1, 2, 3]
+      player_win
+    elsif one == [1, 2, 3, 4]
+      player_win
+    elsif one == [2, 3, 4, 5]
+      player_win
+    elsif one == [3, 4, 5, 6]
+      player_win
+    end
+  end
 
 
   def player_win
