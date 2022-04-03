f9fdf9e0a97df2f5a0b3c76de231bbd9e33d5868
Stephen Wilkens <stephenwilkens@gmail.com>
2022-04-03 14:28:00 -0600

    feat:clean up board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,38 +1,30 @@
 require 'pry'
 
 class Board
-
-
-  attr_reader :hash_columns
+  attr_reader :hash_columns,
 
   def initialize(hash_columns = {
 
-    A: ['.', '.', '.', '.', '.', '.'],
-    B: ['.', '.', '.', '.', '.', '.'],
-    C: ['.', '.', '.', '.', '.', '.'],
-    D: ['.', '.', '.', '.', '.', '.'],
-    E: ['.', '.', '.', '.', '.', '.'],
-    F: ['.', '.', '.', '.', '.', '.'],
-    G: ['.', '.', '.', '.', '.', '.']
+    a: ['.', '.', '.', '.', '.', '.'],
+    b: ['.', '.', '.', '.', '.', '.'],
+    c: ['.', '.', '.', '.', '.', '.'],
+    d: ['.', '.', '.', '.', '.', '.'],
+    e: ['.', '.', '.', '.', '.', '.'],
+    f: ['.', '.', '.', '.', '.', '.'],
+    g: ['.', '.', '.', '.', '.', '.']
     })
     @hash_columns = hash_columns
   end
 
-  def welcome_message
-    return "Welcome, please select a column (A-G)"
-  end
-
-
   def print_board
     (row6,row5,row4,row3,row2,row1,) = @hash_columns.values.transpose
 
-
-    puts " A B C D E F G"
-    p row6.join(' ')
-    p row5.join(' ')
-    p row4.join(' ')
-    p row3.join(' ')
-    p row2.join(' ')
-    p row1.join(' ')
+    puts "A B C D E F G"
+    puts row6.join(' ')
+    puts row5.join(' ')
+    puts row4.join(' ')
+    puts row3.join(' ')
+    puts row2.join(' ')
+    puts row1.join(' ')
   end
 end
