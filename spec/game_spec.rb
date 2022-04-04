e35ded08302ee1d94596d6bac4029d3c23e05295
Stephen Wilkens <stephenwilkens@gmail.com>
2022-04-03 18:42:56 -0600

    Test: add exists test and player_win test

---
+++ spec/game_spec.rb
@@ -0,0 +1,20 @@
+require 'rspec'
+require './lib/messages'
+require './lib/game'
+require 'pry'
+
+describe Game do
+  let(:game){Game.new}
+
+  it "exists" do
+    expect(game).to be_a(Game)
+    # expect(@board).to be_a(Board)
+    # expect(@message).to be_a(Message)
+
+  end
+
+  it 'can execute player_win' do
+    expect(game).to respond_to(:player_win)
+  end
+
+end
